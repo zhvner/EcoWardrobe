8a662de7fbcfd7cf80c1942eae1bbcfbcf3c49d7
Zhanerke <szhanerz@gmail.com>
2023-08-09 18:38:48 -0700

    checkstyle fixed: tests

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -1,4 +1,4 @@
-package ca.ubc.cpsc210.alarm.test;
+package model;
 
 import model.Event;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,10 +12,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Unit tests for the Event class
+ * author: CPSC 210
+ * author: Zhanerke Zhumash
  */
 public class EventTest {
     private Event e;
     private Date d;
+    private Event event1;
+    private Event event2;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
     //is different from time that line (1) is executed.  Lines (1) and (2) must
@@ -25,6 +29,9 @@ public class EventTest {
     public void runBefore() {
         e = new Event("Sensor open at door");   // (1)
         d = Calendar.getInstance().getTime();   // (2)
+
+        event1 = new Event("Description 1");
+        event2 = new Event("Description 1");
     }
 
     @Test
@@ -45,6 +52,32 @@ public class EventTest {
                 new Event("Sensor open at door").hashCode(),
                 new Event("Sensor open at door").hashCode()
         );
+    }
+
+    @Test
+    public void testEqualsWithSameEvent() {
+        assertTrue(event1.equals(event1));
+    }
+
+    @Test
+    public void testEqualsWithEqualEvents() {
+        assertTrue(event1.equals(event2));
+    }
+
+    @Test
+    public void testEqualsWithDifferentEvents() {
+        Event event3 = new Event("Different Description");
+        assertFalse(event1.equals(event3));
+    }
+
+    @Test
+    public void testEqualsWithDifferentObject() {
+        assertFalse(event1.equals("Some other object"));
+    }
+
+    @Test
+    public void testEqualsWithNull() {
+        assertFalse(event1.equals(null));
     }
 
     @Test
